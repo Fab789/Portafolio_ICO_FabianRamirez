006313a96bdc5fe5daab8d1f74e276f9c44a2c3a
Fab192 <framirez90304@ufide.ac.cr>
2025-01-22 20:48:21 -0600

    Semana2

---
+++ portafolioFabián/src/main/java/Portafolio/Portafolio/PortafolioFabianApplication.java
@@ -0,0 +1,13 @@
+package Portafolio.Portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioFabianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioFabianApplication.class, args);
+	}
+
+}
