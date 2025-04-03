f3694bcf46657d1693b90da7ec86b00269db20a9
Fab192 <framirez90304@ufide.ac.cr>
2025-04-02 20:36:08 -0600

    semana 12

---
+++ portafolioFabián/src/main/java/Portafolio/Portafolio/Controller/RegistroController.java
@@ -0,0 +1,22 @@
+
+package Portafolio.Portafolio.Controller;
+
+import Portafolio.Portafolio.domain.Usuario;
+
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+public class RegistroController {
+    
+    
+}
---
+++ portafolioFabián/src/main/java/Portafolio/Portafolio/DAO/RolDao.java
@@ -0,0 +1,8 @@
+
+package Portafolio.Portafolio.DAO;
+import Portafolio.Portafolio.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+    
+}
